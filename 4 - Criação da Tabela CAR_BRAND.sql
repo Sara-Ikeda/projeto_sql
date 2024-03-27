ce4d4e1fa63360b5f264dd56b11199bf6c7a31a2
Sara Ikeda <sara.ikeda10@gmail.com>
2024-03-27 16:29:46 -0300

    4 - Criação/Inserção Tabela CAR_BRAND

---
+++ 4 - Criação da Tabela CAR_BRAND.sql
@@ -0,0 +1,10 @@
+-- SQLite
+-- Criação da tabela tabela CAR_BRAND (marcas)
+CREATE TABLE CAR_BRAND(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(32)
+);
+
+INSERT INTO CAR_BRAND (BRAND_NAME) 
+    VALUES ('Chevrolet'),('Toyota'),('Hyundai'),('Volkswagen'),
+           ('Jeep'),('Renault'),('Honda'),('Fiat');
